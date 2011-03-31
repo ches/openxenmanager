3b8a07091742d1afa4fab441dbdf5c01f8695e5f
owenrhun <owenrhun@46ce7bd2-b275-43e2-a15a-3c64a95c893c>
2011-03-31 20:28:11 +0000

    fixed typo
    git-svn-id: https://openxenmanager.svn.sourceforge.net/svnroot/openxenmanager@63 46ce7bd2-b275-43e2-a15a-3c64a95c893c

--- oxcSERVER.py
+++ oxcSERVER.py
@@ -1380,10 +1380,10 @@ class oxcSERVER(oxcSERVERvm,oxcSERVERhost,oxcSERVERproperties,oxcSERVERstorage,o
         if "devserial" in self.all_storage[ref]['sm_config']:
             devserial =  self.all_storage[ref]['sm_config']['devserial'].split("-",2)
             labels['lblstgserial'] =  devserial[0].upper() + " ID:"
-        if len(devserial) > 1:
-                labels['lblstgscsi'] = devserial[1]
-        else:
-            labels['lblstgscsi'] = devserial[0]
+            if len(devserial) > 1:
+                    labels['lblstgscsi'] = devserial[1]
+            else:
+                labels['lblstgscsi'] = devserial[0]
         else:
             labels['lblstgscsi'] = ""
       
